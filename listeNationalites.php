823830e0037a7a6988c88298505e27d478e13d1a
luka <heneault.luka@gmail.com>
2025-02-03 08:47:53 +0100

    dqz

--- listeNationalites.php
+++ listeNationalites.php
@@ -1,9 +1,16 @@
 <?php include "header.php";
 include "connexionPdo.php";
+// liste des nationalités
 $req=$monPdo->prepare("select n.num, n.libelle as 'libNation', c.libelle as 'libContinent' from nationalite n, continent c where n.numContinent=c.num order by n.libelle");
 $req->setFetchMode(PDO::FETCH_OBJ);
 $req->execute();
 $lesNationalites=$req->fetchALL();
+
+// liste des continents
+$reqContinent=$monPdo->prepare("select * from continent");
+$reqContinent->setFetchMode(PDO::FETCH_OBJ);
+$reqContinent->execute();
+$lesContinent=$reqContinent->fetchALL();
 
 if(!empty($_SESSION['message'])){
     $mesMessages=$_SESSION['message'];
@@ -21,12 +28,32 @@ if(!empty($_SESSION['message'])){
 
 ?>
 
-
 <div class="container mt-5">
     <div class="row pt-3">
         <div class="col-9"><h2>Liste des nationalites</h2></div>
         <div class="col-3"><a href="formNationalite.php?action=Ajouter" class='btn btn-success'><i class="fas fa-plus-circle"></i> Créer une nationalité</a></div>
     </div>
+
+        <form action="" method="get" class="border border-primary rounded p-3 mt-3 mb-3">
+            <div class="row">
+                <div class="col">
+                <input type="text" class='form-control' id='libelle' placehorder='Saisir le libellé' name='libelle' value="<?php if($action == "Modifier") {echo $laNationalite->libelle;} ?>">
+                </div>
+                <div class="col">
+                    <select name="continent" class="form-control">
+                        <?php
+                        foreach($lesContinent as $continent){
+                            $selection=$continent->num == $laNationalite-> numContinent ? 'selected' : '';
+                            echo "<option value='$continent->num' $selection>$continent->libelle</option>";
+                        }
+                        ?>
+                    </select>
+                </div>
+                <div class="col">
+                    <button type="submit" class="btn btn-success btn-block">Rechercher</button>
+                </div>
+            </div>
+        </form>
 
     <table class="table table-hover table-striped">
         <thead class="thead-dark">
@@ -41,17 +68,17 @@ if(!empty($_SESSION['message'])){
             <?php
                 foreach($lesNationalites as $nationalite){
                     echo "<tr>";
-                        echo "<td class='col-md-2'>$nationalite->num</td>";
-                        echo "<td class='col-md-5'>$nationalite->libNation</td>";
-                        echo "<td class='col-md-3'>$nationalite->libContinent</td>";
-                        echo "<td class='col-md-2'>
-                            <a href='formNationalite.php?action=Modifier&num=$nationalite->num' class='btn btn-primary'><i class='fas fa-pen'></i></a>
-                            <a href='#modalSuppression' data-toggle='modal' data-message='Voulez-vous supprimer cette nationalite ?' data-suppression='supprimerNationalite.php?num=$nationalite->num' class='btn btn-danger'><i class='fas fa-trash'></i></a>
-                        </td>";
+                    echo "<td class='col-md-2'>$nationalite->num</td>";
+                    echo "<td class='col-md-5'>$nationalite->libNation</td>";
+                    echo "<td class='col-md-3'>$nationalite->libContinent</td>";
+                    echo "<td class='col-md-2'>
+                    <a href='formNationalite.php?action=Modifier&num=$nationalite->num' class='btn btn-primary'><i class='fas fa-pen'></i></a>
+                    <a href='#modalSuppression' data-toggle='modal' data-message='Voulez-vous supprimer cette nationalite ?' data-suppression='supprimerNationalite.php?num=$nationalite->num' class='btn btn-danger'><i class='fas fa-trash'></i></a>
+                    </td>";
                     echo "</tr>";
                 }
                 // supprimerNationalite.php?num=$nationalite->num'
-            ?>
+                ?>
         </tbody>
     </table>
 </div>
